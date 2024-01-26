c38d7e9373ee2a67a8212bfa90c97fb637bc4026
AkaTordu <maximilienlandes@gmail.com>
2024-01-26 09:30:41 +0100

    first consolelogs

--- exos.js
+++ exos.js
@@ -1,13 +1,12 @@
-/* EXERCICE 1
-Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. 
-No floats or non-positive integers will be passed.
-For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
-[10, 343445353, 3453445, 3453545353453] should return 3453455.
-*/
+let age = 25;               // Utilisation de let
+console.log("Votre âge est : " + age);
+const name = "Alex";        // Utilisation de const
+let message = "Bonjour";    // String
+let score = 100;            // Number
+let isActive = true;        // Boolean
 
-function sumTwoSmallestNumbers(numbers) {
-    
-    numbers.sort((a, b) => a - b);
+let x = 10;
+let y = 5;
+let z = x + y;  // 15
 
-    return numbers[0] + numbers[1];
-}
+console.log("Bonjour "+ name + ", vous avez " + age +" ans");
