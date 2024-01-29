acb3760e641aea289637af498cd0662141f5ada0
AkaTordu <maximilienlandes@gmail.com>
2024-01-29 17:00:50 +0100

    Hunt Update

--- Hunt.js
+++ Hunt.js
@@ -1,9 +1,21 @@
+/*
 let exemple = {
     xp: "0",
     prime: "0"
 }
+*/
+
+let indice = {
+    xp: "0",
+    prime: "50"
+}
 
 let goule = {
-    xp: "0",
+    xp: "20",
+    prime: "0"
+}
+
+let ruche = {
+    xp: "60",
     prime: "0"
 }
