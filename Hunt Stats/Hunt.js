f4a2ed94c16c0768ca319d5994e8b5010ae52d49
AkaTordu <maximilienlandes@gmail.com>
2024-01-29 19:36:57 +0100

    Update Projects

--- Hunt Stats/Hunt.js
+++ Hunt Stats/Hunt.js
@@ -10,11 +10,25 @@ let indice = {
     prime: "50"
 }
 
+// PVP - PLAYER KILLS
 let hunterOneStar = {
     xp: "0",
     prime: "0"
 }
+let hunterTwoStar = {
+    xp: "0",
+    prime: "0"
+}
+let hunterThreeStar = {
+    xp: "0",
+    prime: "0"
+}
+let hunterFourStar = {
+    xp: "0",
+    prime: "0"
+}
 
+// PVE - VERSUS WILD
 let goule = {
     xp: "20",
     prime: "0"
