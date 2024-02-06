2e460af1007b4a73bae7f68b487c799f4eee7bf4
AkaTordu <maximilienlandes@gmail.com>
2024-02-06 18:31:15 +0100

    images update

--- huntstats/src/GameInterface.js
+++ huntstats/src/GameInterface.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { gameElements } from './GameElements.js'; // Assurez-vous que le chemin d'importation est correct
+import { gameElements } from './GameElements';
 
 const GameInterface = () => {
     const [nomMonstre, setNomMonstre] = useState('');
     const [nombre, setNombre] = useState(0);
     const [resultat, setResultat] = useState({ xp: 0, prime: 0 });
+    const [image, setImage] = useState('');
 
     const calculerResultat = () => {
         const element = gameElements[nomMonstre];
@@ -12,8 +13,10 @@ const GameInterface = () => {
             const xpTotale = element.xp * nombre;
             const primeTotale = element.prime * nombre;
             setResultat({ xp: xpTotale, prime: primeTotale });
+            setImage(element.image); // Met à jour l'image
         } else {
             alert('Ce monstre n\'existe pas.');
+            setImage(''); // Réinitialise l'image si le monstre n'existe pas
         }
     };
 
@@ -21,15 +24,16 @@ const GameInterface = () => {
         <div>
             <h2>Calculateur de Récompenses</h2>
             <div>
-                <label htmlFor="monstre">Choisissez un monstre : </label>
+                <label htmlFor="monstre">Choisissez un monstre:</label>
                 <select id="monstre" value={nomMonstre} onChange={e => setNomMonstre(e.target.value)}>
+                    <option value="">Sélectionnez un monstre</option>
                     {Object.keys(gameElements).map(monstre => (
                         <option key={monstre} value={monstre}>{monstre}</option>
                     ))}
                 </select>
             </div>
             <div>
-                <label htmlFor="nombre">Nombre tués : </label>
+                <label htmlFor="nombre">Nombre tués:</label>
                 <input
                     id="nombre"
                     type="number"
@@ -39,8 +43,9 @@ const GameInterface = () => {
             </div>
             <button onClick={calculerResultat}>Calculer</button>
             <div>
-                <p>Total XP: {resultat.xp}, Total Prime : {resultat.prime}</p>
+                <p>Total XP: {resultat.xp}, Total Prime: {resultat.prime}</p>
             </div>
+            {image && <img src={image} alt="Monstre" style={{ maxWidth: '200px', marginTop: '20px' }} />}
         </div>
     );
 };
