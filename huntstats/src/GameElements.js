435b72cbe4756c725fbdfbb87a90c319e0f5b787
AkaTordu <maximilienlandes@gmail.com>
2024-02-26 10:38:04 +0100

    documentation gameElements

--- huntstats/src/GameElements.js
+++ huntstats/src/GameElements.js
@@ -1,15 +1,15 @@
 export const gameElements = {
-    indice: { xp: 0, prime: 50, image: '/images/Indices/PremierIndice.png' },
-    repaire_localise: { xp: 0, prime: 25, image: '/images/exemple.png'  },
-    boss_tue: { xp: 0, prime: 25, image: '/images/exemple.png'  },
-    contrat_rempli: { xp: 0, prime: 900, image: '/images/exemple.png' },
+    indice: { xp: 0, prime: 50, image: '/images/Indices/PremierIndice.png' }, // bonus a chaque indice
+    repaire_localise: { xp: 0, prime: 25, image: '/images/exemple.png'  }, // Localisation du repaire grâce aux indices ou découverte directe, fonctionne pour les deux boss
+    boss_tue: { xp: 0, prime: 25, image: '/images/exemple.png'  }, // Fonctionne pour chaque boss
+    contrat_rempli: { xp: 0, prime: 900, image: '/images/exemple.png' }, // 
     bonus_place_nette: { xp: 0, prime: 25, image: '/images/exemple.png' }, // localiser - Tuer - Extraction (bonus obtenable une fois par boss)
     bonus_victoire_integrale: { xp: 0, prime: 25, image: '/images/exemple.png' }, // Toutes les primes de la carte
     hunterOneStar: { xp: 0, prime: 0, image: '/images/exemple.png'  }, // Player kill 1 étoile
     hunterTwoStar: { xp: 0, prime: 0, image: '/images/exemple.png'  }, // Player kill 2 étoiles
     hunterThreeStar: { xp: 450, prime: 0, image: '/images/exemple.png'  }, // Player kill 3 étoiles
     hunterFourStar: { xp: 0, prime: 0, image: '/images/exemple.png'  }, // Player kill 4 étoiles
-    reanimation: { xp: 100, prime: 0, image: '/images/exemple.png'  },
+    reanimation: { xp: 100, prime: 0, image: '/images/exemple.png'  }, // chaque interaction rapporte les quotas
     goule: { xp: 20, prime: 0, image: '/images/exemple.png'  },
     ruche: { xp: 60, prime: 0, image: '/images/exemple.png'  },
     cerbere: { xp: 40, prime: 0, image: '/images/exemple.png'  },
