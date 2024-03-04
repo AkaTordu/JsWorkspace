05093d38895cfd5f828b10313e135f9aaada0895
AkaTordu <maximilienlandes@gmail.com>
2024-03-04 22:25:09 +0100

    update elements

--- huntstats/src/GameElements.js
+++ huntstats/src/GameElements.js
@@ -2,6 +2,7 @@ export const gameElements = {
     indice: { xp: 0, prime: 50, image: '/images/Indices/PremierIndice.png' },
     repaire_localise: { xp: 0, prime: 25, image: '/images/exemple.png'  },
     boss_tue: { xp: 0, prime: 25, image: '/images/exemple.png'  },
+    expulse: { xp: 0, prime: 300, image: '/images/exemple.png'  }, // Banissement du boss
     contrat_rempli: { xp: 0, prime: 900, image: '/images/exemple.png' },
     bonus_place_nette: { xp: 0, prime: 25, image: '/images/exemple.png' }, // localiser - Tuer - Extraction (bonus obtenable une fois par boss)
     bonus_victoire_integrale: { xp: 0, prime: 25, image: '/images/exemple.png' }, // Toutes les primes de la carte
@@ -9,9 +10,9 @@ export const gameElements = {
     hunterTwoStar: { xp: 0, prime: 0, image: '/images/exemple.png'  }, // Player kill 2 étoiles
     hunterThreeStar: { xp: 450, prime: 0, image: '/images/exemple.png'  }, // Player kill 3 étoiles
     hunterFourStar: { xp: 0, prime: 0, image: '/images/exemple.png'  }, // Player kill 4 étoiles
-    reanimation: { xp: 100, prime: 0, image: '/images/exemple.png'  },
-    goule: { xp: 20, prime: 0, image: '/images/exemple.png'  },
-    ruche: { xp: 60, prime: 0, image: '/images/exemple.png'  },
+    reanimation: { xp: 100, prime: 0, image: '/images/exemple.png'  }, // Pour chaque réa
+    goule: { xp: 20, prime: 0, image: '/images/exemple.png'  }, // Normal mob
+    ruche: { xp: 60, prime: 0, image: '/images/exemple.png'  }, 
     cerbere: { xp: 40, prime: 0, image: '/images/exemple.png'  },
     cuirasse: { xp: 60, prime: 0, image: '/images/exemple.png'  },
     immolateur: { xp: 60, prime: 0, image: '/images/exemple.png'  },
