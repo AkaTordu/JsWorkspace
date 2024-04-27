d9a61a206fa3dc1970f2dcb532027ee4d53dd3e4
AkaTordu <maximilienlandes@gmail.com>
2024-04-27 16:24:19 +0200

    tries with tailwindcss

--- huntnext/tailwind.config.ts
+++ huntnext/tailwind.config.ts
@@ -8,13 +8,60 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#0056b3',
+        background: '#f7f7f7',
+        text: '#333',
+        'button-hover': '#003870',
       },
+      borderRadius: {
+        custom: '8px',
+      },
+      boxShadow: {
+        custom: '0 4px 6px rgba(0,0,0,0.1)',
+        'custom-light': '0 2px 5px rgba(0,0,0,0.05)',
+        'custom-strong': '0 5px 15px rgba(0,0,0,0.1)',
+        'custom-highlight': '0 2px 10px rgba(0,0,0,0.2)',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      maxWidth: {
+        container: '960px',
+      },
+      spacing: {
+        '7.5': '30px', // Equivalent à 30px mentionné pour le body
+        '20': '20px', // Généralement utilisé pour les padding/margin
+      },
+      transitionProperty: {
+        'height-color-shadow': 'height, color, box-shadow',
+        'bgc-filter-opacity': 'background-color, filter, opacity',
+      },
+      transitionTimingFunction: {
+        'in-out-linear': 'ease-in-out',
+      },
+      transitionDuration: {
+        '300': '300ms',
+        '3000': '3000ms',
+      },
+      filter: { // Assure-toi que Tailwind CSS supporte les filtres directement ou utilise un plugin si nécessaire
+        'none': 'none',
+        'grayscale': 'grayscale(100%)',
+      },
+      extend: {
+        animation: {
+          colorFade: 'colorFade 10s ease-in-out infinite',
+        },
+        keyframes: {
+          colorFade: {
+            '0%, 100%': { backgroundColor: 'white', color: 'black' },
+            '50%': { backgroundColor: 'black', color: 'white' },
+          },
+        },
+      }
     },
   },
   plugins: [],
 };
+
 export default config;
